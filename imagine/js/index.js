5d17fdb8e15011897573532dd96a6fa0851c768d
Mohammad Rezvani <mohammadrezvani2002@gmail.com>
2022-10-03 23:37:00 +0330

    14th Edition

--- imagine/js/index.js
+++ imagine/js/index.js
@@ -1,4 +1,6 @@
 let activeComponent = 'none' ;
+let activeComponentType ;
+
 const closeOffcanvas = document.querySelector('.closeOffcanvas')
 const closeModal = document.querySelector('.closeModal')
 
@@ -15,12 +17,14 @@ const offcanvasDisplay = (target) => {
         document.body.classList.toggle('stop-scrolling')
         setTimeout (() => {
             activeComponent = target ;
+            activeComponentType = 'offcanvas' ;
          },100)
 
     }else if(display == 'block'){
         target.style.display = "none";
         document.body.classList.toggle('stop-scrolling')
         activeComponent = 'none';
+        activeComponentType = 'none' ;
     }
 }
 
@@ -28,15 +32,20 @@ const ModalDisplay = (target) => {
     const display = getComputedStyle(target).display;
     if (display == 'none'){
          target.style.display = "block";
-        //  document.body.style.opacity = '0.5';
-        //  target.style.opacity = '1';
+         
+        //  document.body.style.opacity = '0.3';
+        //  target.classList.toggle('opacity-full');
+
          setTimeout (() => {
             activeComponent = target ;
+            activeComponentType = 'modal' ;
+
          },100)
     }else if(display == 'block'){
         target.style.display = "none";
         document.body.style.opacity = '1';
         activeComponent = 'none';
+        activeComponentType = 'none' ;
 
 
     }
@@ -44,8 +53,18 @@ const ModalDisplay = (target) => {
     window.addEventListener('click', function(e){   
         if (activeComponent !== 'none'){
             if (!activeComponent.contains(e.target) || closeModal.contains(e.target) || closeOffcanvas.contains(e.target) ){
+
+                if (activeComponentType == 'offcanvas') {
+                     document.body.classList.toggle('stop-scrolling')
+                }else if (activeComponentType == 'modal') {
+                     document.body.style.opacity = '1';
+                }
+
                 activeComponent.style.display = "none";
                 activeComponent = 'none';
+                activeComponentType = 'none';
             }
         }
       });
+
+  
