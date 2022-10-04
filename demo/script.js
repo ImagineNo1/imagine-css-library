837f3eb9d8d9fdb0cf1955802f64f1866d2ef567
Mohammad Rezvani <mohammadrezvani2002@gmail.com>
2022-10-05 03:13:11 +0330

    18th Edition

---
+++ demo/script.js
@@ -0,0 +1,212 @@
+document.querySelector('#navbar').style.border = "3px solid black";
+
+const navbarShow = () => {
+    document.querySelector('#navbarC').style.display = 'block';
+    document.querySelector('#navbar').style.border = "3px solid black";
+    document.querySelector('#offcanvasC').style.display = 'none';
+    document.querySelector('#modalsC').style.display = 'none';
+    document.querySelector('#gridsystemC').style.display = 'none'
+    document.querySelector('#utilitiesC').style.display = 'none';
+    document.querySelector('#cardC').style.display = 'none';
+    document.querySelector('#FormsC').style.display = 'none';
+    document.querySelector('#ButtonsC').style.display = 'none';
+    document.querySelector('#closeC').style.display = 'none';
+
+    document.querySelector('#offcanvas').style.border = "";
+    document.querySelector('#modals').style.border = "";
+    document.querySelector('#gridsystem').style.border = "";
+    document.querySelector('#utilities').style.border = "";
+    document.querySelector('#card').style.border = "";
+    document.querySelector('#Forms').style.border = "";
+    document.querySelector('#Buttons').style.border = "";
+    document.querySelector('#close').style.border = "";
+
+}
+const offcanshow = () => {
+    document.querySelector('#navbarC').style.display = 'none';
+    document.querySelector('#offcanvasC').style.display = 'block';
+    document.querySelector('#offcanvas').style.border = "3px solid black";
+    document.querySelector('#modalsC').style.display = 'none';
+    document.querySelector('#gridsystemC').style.display = 'none'
+    document.querySelector('#utilitiesC').style.display = 'none';
+    document.querySelector('#cardC').style.display = 'none';
+    document.querySelector('#FormsC').style.display = 'none';
+    document.querySelector('#ButtonsC').style.display = 'none';
+    document.querySelector('#closeC').style.display = 'none';
+
+    document.querySelector('#navbar').style.border = "";
+    document.querySelector('#modals').style.border = "";
+    document.querySelector('#gridsystem').style.border = "";
+    document.querySelector('#utilities').style.border = "";
+    document.querySelector('#card').style.border = "";
+    document.querySelector('#Forms').style.border = "";
+    document.querySelector('#Buttons').style.border = "";
+    document.querySelector('#close').style.border = "";
+}
+const modalshow = () => {
+    document.querySelector('#navbarC').style.display = 'none';
+    document.querySelector('#offcanvasC').style.display = 'none';
+    document.querySelector('#modalsC').style.display = 'block';
+    document.querySelector('#modals').style.border = "3px solid black";
+    document.querySelector('#utilitiesC').style.display = 'none';
+    document.querySelector('#cardC').style.display = 'none';
+    document.querySelector('#FormsC').style.display = 'none';
+    document.querySelector('#ButtonsC').style.display = 'none';
+    document.querySelector('#closeC').style.display = 'none';
+
+    document.querySelector('#offcanvas').style.border = "";
+    document.querySelector('#navbar').style.border = "";
+    document.querySelector('#gridsystem').style.border = "";
+    document.querySelector('#utilities').style.border = "";
+    document.querySelector('#card').style.border = "";
+    document.querySelector('#Forms').style.border = "";
+    document.querySelector('#Buttons').style.border = "";
+    document.querySelector('#close').style.border = "";
+}
+const gridshow = () => {
+    document.querySelector('#navbarC').style.display = 'none';
+    document.querySelector('#offcanvasC').style.display = 'none';
+    document.querySelector('#modalsC').style.display = 'none';
+    document.querySelector('#gridsystemC').style.display = 'block'
+    document.querySelector('#gridsystem').style.border = "3px solid black";
+    document.querySelector('#utilitiesC').style.display = 'none';
+    document.querySelector('#cardC').style.display = 'none';
+    document.querySelector('#FormsC').style.display = 'none';
+    document.querySelector('#ButtonsC').style.display = 'none';
+    document.querySelector('#closeC').style.display = 'none';
+
+    document.querySelector('#offcanvas').style.border = "";
+    document.querySelector('#modals').style.border = "";
+    document.querySelector('#navbar').style.border = "";
+    document.querySelector('#utilities').style.border = "";
+    document.querySelector('#card').style.border = "";
+    document.querySelector('#Forms').style.border = "";
+    document.querySelector('#Buttons').style.border = "";
+    document.querySelector('#close').style.border = "";
+}
+const utilityshow = () => {
+    document.querySelector('#navbarC').style.display = 'none';
+    document.querySelector('#offcanvasC').style.display = 'none';
+    document.querySelector('#modalsC').style.display = 'none';
+    document.querySelector('#gridsystemC').style.display = 'none'
+    document.querySelector('#utilitiesC').style.display = 'block';
+    document.querySelector('#utilities').style.border = "3px solid black";
+    document.querySelector('#cardC').style.display = 'none';
+    document.querySelector('#FormsC').style.display = 'none';
+    document.querySelector('#ButtonsC').style.display = 'none';
+    document.querySelector('#closeC').style.display = 'none';
+
+    document.querySelector('#offcanvas').style.border = "";
+    document.querySelector('#modals').style.border = "";
+    document.querySelector('#gridsystem').style.border = "";
+    document.querySelector('#navbar').style.border = "";
+    document.querySelector('#card').style.border = "";
+    document.querySelector('#Forms').style.border = "";
+    document.querySelector('#Buttons').style.border = "";
+    document.querySelector('#close').style.border = "";
+}
+const cardshow = () => {
+    document.querySelector('#navbarC').style.display = 'none';
+    document.querySelector('#offcanvasC').style.display = 'none';
+    document.querySelector('#modalsC').style.display = 'none';
+    document.querySelector('#gridsystemC').style.display = 'none'
+    document.querySelector('#utilitiesC').style.display = 'none';
+    document.querySelector('#cardC').style.display = 'block';
+    document.querySelector('#card').style.border = "3px solid black";
+    document.querySelector('#FormsC').style.display = 'none';
+    document.querySelector('#ButtonsC').style.display = 'none';
+    document.querySelector('#closeC').style.display = 'none';
+
+    document.querySelector('#offcanvas').style.border = "";
+    document.querySelector('#modals').style.border = "";
+    document.querySelector('#gridsystem').style.border = "";
+    document.querySelector('#utilities').style.border = "";
+    document.querySelector('#navbar').style.border = "";
+    document.querySelector('#Forms').style.border = "";
+    document.querySelector('#Buttons').style.border = "";
+    document.querySelector('#close').style.border = "";
+}
+const formshow = () => {
+    document.querySelector('#navbarC').style.display = 'none';
+    document.querySelector('#offcanvasC').style.display = 'none';
+    document.querySelector('#modalsC').style.display = 'none';
+    document.querySelector('#gridsystemC').style.display = 'none'
+    document.querySelector('#utilitiesC').style.display = 'none';
+    document.querySelector('#cardC').style.display = 'none';
+    document.querySelector('#FormsC').style.display = 'block';
+    document.querySelector('#Forms').style.border = "3px solid black";
+    document.querySelector('#ButtonsC').style.display = 'none';
+    document.querySelector('#closeC').style.display = 'none';
+
+    document.querySelector('#offcanvas').style.border = "";
+    document.querySelector('#modals').style.border = "";
+    document.querySelector('#gridsystem').style.border = "";
+    document.querySelector('#utilities').style.border = "";
+    document.querySelector('#card').style.border = "";
+    document.querySelector('#navbar').style.border = "";
+    document.querySelector('#Buttons').style.border = "";
+    document.querySelector('#close').style.border = "";
+}
+const buttonshow = () => {
+    document.querySelector('#navbarC').style.display = 'none';
+    document.querySelector('#offcanvasC').style.display = 'none';
+    document.querySelector('#modalsC').style.display = 'none';
+    document.querySelector('#gridsystemC').style.display = 'none'
+    document.querySelector('#utilitiesC').style.display = 'none';
+    document.querySelector('#cardC').style.display = 'none';
+    document.querySelector('#FormsC').style.display = 'none';
+    document.querySelector('#ButtonsC').style.display = 'block';
+    document.querySelector('#Buttons').style.border = "3px solid black";
+    document.querySelector('#closeC').style.display = 'none';
+
+    document.querySelector('#offcanvas').style.border = "";
+    document.querySelector('#modals').style.border = "";
+    document.querySelector('#gridsystem').style.border = "";
+    document.querySelector('#utilities').style.border = "";
+    document.querySelector('#card').style.border = "";
+    document.querySelector('#Forms').style.border = "";
+    document.querySelector('#navbar').style.border = "";
+    document.querySelector('#close').style.border = "";
+}
+const closeeshow = () => {
+    document.querySelector('#navbarC').style.display = 'none';
+    document.querySelector('#offcanvasC').style.display = 'none';
+    document.querySelector('#modalsC').style.display = 'none';
+    document.querySelector('#gridsystemC').style.display = 'none'
+    document.querySelector('#utilitiesC').style.display = 'none';
+    document.querySelector('#cardC').style.display = 'none';
+    document.querySelector('#FormsC').style.display = 'none';
+    document.querySelector('#ButtonsC').style.display = 'none';
+    document.querySelector('#closeC').style.display = 'block';
+    document.querySelector('#close').style.border = "3px solid black";
+
+    document.querySelector('#offcanvas').style.border = "";
+    document.querySelector('#modals').style.border = "";
+    document.querySelector('#gridsystem').style.border = "";
+    document.querySelector('#utilities').style.border = "";
+    document.querySelector('#card').style.border = "";
+    document.querySelector('#Forms').style.border = "";
+    document.querySelector('#Buttons').style.border = "";
+    document.querySelector('#navbar').style.border = "";
+}
+
+const navbar = document.querySelector('#navbar')
+const offcanvas = document.querySelector('#offcanvas')
+const modals = document.querySelector('#modals')
+const gridsystem = document.querySelector('#gridsystem')
+const utilities = document.querySelector('#utilities')
+const card = document.querySelector('#card')
+const Forms = document.querySelector('#Forms')
+const Buttons = document.querySelector('#Buttons')
+const closee = document.querySelector('#close')
+
+navbar.addEventListener('click',navbarShow)
+offcanvas.addEventListener('click',offcanshow)
+modals.addEventListener('click',modalshow)
+gridsystem.addEventListener('click',gridshow)
+utilities.addEventListener('click',utilityshow)
+card.addEventListener('click',cardshow)
+Forms.addEventListener('click',formshow)
+Buttons.addEventListener('click',buttonshow)
+closee.addEventListener('click',closeeshow)
+
